04cc71996def9833eac0779ef0a9ea8438236835
playforfunny <playforfun.mix@gmail.com>
2016-07-17 18:04:21 +0300

    internship
    internship

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Internship</title>
+</head>
+<body>
+
+<?php define('ROOT', dirname(__FILE__));?>
+
+<?php include ROOT . '/pageparts/microsoft.html' ?>
+<?php include ROOT . '/pageparts/dropbox.html' ?>
+<?php include ROOT . '/pageparts/invision.html' ?>
+<?php include ROOT . '/pageparts/oculus.html' ?>
+<?php include ROOT . '/pageparts/apple.html' ?>
+
+
+
+</body>
+</html>
